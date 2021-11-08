b1bbc7378e0e81e80ab32ce68f2fd252e437c87d
shlyapo <72437851+shlyapo@users.noreply.github.com>
2021-11-08 23:01:45 +0300

    Add files via upload

---
+++ 053501_Shlyapo_Lab9.Domain/Firma.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace _053501_Shlyapo_Lab9.Domain
+{
+    [Serializable]
+    public class Firma
+    {
+        public string Name { get; set; }
+        public List<Otdel> People;
+
+        public Firma() { }
+        public Firma(string Name)
+        {
+            this.Name = Name;
+            People = new List<Otdel>();
+        }
+        public Firma(string Name, List<Otdel> p)
+        {
+            this.Name = Name;
+            People = p;
+        }
+    }
+}
---
+++ 053501_Shlyapo_Lab9.Domain/ISerializer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace _053501_Shlyapo_Lab9.Domain
+{
+    public interface ISerializer
+    {
+        IEnumerable<Firma> DeSerializeByLINQ(string fileName);
+        IEnumerable<Firma> DeSerializeXML(string fileName);
+        IEnumerable<Firma> DeSerializeJSON(string fileName);
+        void SerializeByLINQ(IEnumerable<Firma> xxx, string fileName);
+        void SerializeXML(IEnumerable<Firma> xxx, string fileName);
+        void SerializeJSON(IEnumerable<Firma> xxx, string fileName);
+    }
+}
